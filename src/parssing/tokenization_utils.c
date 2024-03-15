307a8d3682604c496b3fd721b7fefa311fa38338
Lilien Auger <lauger@2A6.42angouleme.fr>
2024-03-15 08:45:40 +0100

    feat: new folder parssing of yvann

---
+++ src/parssing/tokenization_utils.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenization_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lauger <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 08:42:13 by lauger            #+#    #+#             */
+/*   Updated: 2024/03/15 08:42:26 by lauger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	is_special_char(char c)
+{
+	return (c == '>' || c == '<' || c == '|' || c == ';' \
+	|| c == '\'' || c == '"' || c == '$' || c == '?');
+}
+
+void	add_special_token(const char **input, t_token **head, t_token_type type)
+{
+	char	value[2];
+
+	value[0] = **input;
+	value[1] = '\0';
+	add_token(head, init_token(type, value));
+	(*input)++;
+}
+
+void	add_word_token(const char **input, t_token **head)
+{
+	char	*word;
+	char	*temp;
+	char	to_add[2];
+
+	word = NULL;
+	word = ft_strdup("");
+	while (**input && !ft_isspace(**input) && !is_special_char(**input))
+	{
+		to_add[0] = **input;
+		to_add[1] = '\0';
+		temp = word;
+		word = ft_strjoin(word, to_add);
+		free(temp);
+		(*input)++;
+	}
+	if (word != NULL)
+	{
+		add_token(head, init_token(TOKEN_WORD, word));
+		free(word);
+	}
+}
+
