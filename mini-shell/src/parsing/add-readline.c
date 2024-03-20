f0841a11c5fd08f098429f594774961b438ca12e
Lilien Auger <lauger@2A6.42angouleme.fr>
2024-03-20 09:00:12 +0100

    fix:rebase master

--- mini-shell/src/parsing/add-readline.c
+++ mini-shell/src/parsing/add-readline.c
@@ -3,35 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   add-readline.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lauger <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: lauger <lauger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 13:16:29 by lauger            #+#    #+#             */
-/*   Updated: 2024/03/18 13:48:24 by lauger           ###   ########.fr       */
+/*   Updated: 2024/03/19 13:09:20 by lauger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int	main(void)
+#define MAX_HISTORY_SIZE 100
+
+void	read_input_two(char *input, char *history[MAX_HISTORY_SIZE], int *history_index)
+{
+	if (strcmp(input, "") != 0)
+	{
+		add_history(input);
+		if (history[*history_index] != NULL)
+			free(history[*history_index]);
+		history[*history_index] = strdup(input);
+		*history_index = (*history_index + 1) % MAX_HISTORY_SIZE;
+	}
+	return ;
+}
+
+int	read_input(void)
 {
 	char	*input;
+	char	*history[MAX_HISTORY_SIZE] = {NULL};
+	int		history_index;
+	int		i;
 
+	history_index = 0;
+	i = 0;
 	while (1)
 	{
-		printf("minishell > ");
-		input = readline("");
-		//printf("-%s-", input);
-		if (input == NULL || strcmp(input, "") == 0)
+		input = readline("minishell > ");
+		if (!input)
 		{
-			printf("-%s-", input);
-			printf("\n");
-			free(input);
-			break;
+			break ;
 		}
-
+		read_input_two(input, (history), &history_index);
+		if (strcmp(input, "exit") == 0)
+		{
+			if (input)
+				free(input);
+			break ;
+		}
+		if (input)
+			free(input);
 	}
+	while (i < MAX_HISTORY_SIZE)
+		if (history[i] != NULL)
+			free(history[i++]);
+	return (0);
+}
+
+int	main(void)
+{
+	read_input();
 	return (0);
 }
