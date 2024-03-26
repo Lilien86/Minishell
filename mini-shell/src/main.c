1d764a62824bd18ab10b953950538bde37a56d03
yvann-hub <barbot.yvann@gmail.com>
2024-03-26 15:07:24 +0100

    Refactor: Add debug fun for tokens print

--- mini-shell/src/main.c
+++ mini-shell/src/main.c
@@ -5,3 +5,17 @@ int	main(void)
 	read_input();
 	return (0);
 }
+
+// #include "../minishell.h"
+
+// // Assurez-vous que cette fonction est déclarée dans minishell.h si vous souhaitez l'utiliser à l'extérieur de ce fichier.
+
+// void	debug_print_tokens(t_token *tokens) {
+//     t_token *current = tokens;
+//     ft_printf("Début de l'affichage des tokens:\n");
+//     while (current != NULL) {
+//         ft_printf("Type de Token: %d, Valeur: %s\n", current->type, current->value);
+//         current = current->next;
+//     }
+//     ft_printf("Fin de l'affichage des tokens.\n");
+// }
