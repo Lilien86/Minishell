8c6801840e28d6bf32fe9c96acec847e216e55b0
yvann-hub <barbot.yvann@gmail.com>
2024-03-27 12:58:28 +0100

    Fix: Handle unclosed token in tokenisation part

--- mini-shell/src/parsing/tokenization.c
+++ mini-shell/src/parsing/tokenization.c
@@ -1,8 +1,18 @@
 #include "../minishell.h"
 
-t_token	*init_token(t_token_type type, char *value)
+/**
+ * @brief Initializes a token with the given type and value.
+ *
+ * This function allocates memory for a new token, sets its type and value,
+ * and initializes the next pointer to NULL.
+ *
+ * @param type The type of the token.
+ * @param value The value of the token.
+ * @return A pointer to the newly initialized token, or NULL if memory allocation fails.
+ */
+t_token *init_token(t_token_type type, char *value)
 {
-	t_token	*token;
+	t_token *token;
 
 	token = (t_token *)malloc(sizeof(t_token));
 	if (!token)
@@ -13,9 +23,15 @@ t_token	*init_token(t_token_type type, char *value)
 	return (token);
 }
 
-void	add_token(t_token **head, t_token *new_token)
+/**
+ * @brief Adds a token to the end of the token list.
+ *
+ * @param head The head of the token list.
+ * @param new_token The token to be added.
+ */
+void add_token(t_token **head, t_token *new_token)
 {
-	t_token	*temp;
+	t_token *temp;
 
 	if (!*head)
 		*head = new_token;
@@ -28,23 +44,35 @@ void	add_token(t_token **head, t_token *new_token)
 	}
 }
 
-void	identify_double_char_tokens(const char **input, t_token **head)
+/**
+ * @brief Identifies and adds double character tokens to the token list.
+ *
+ * @param input The input string.
+ * @param head The head of the token list.
+ */
+void identify_double_char_tokens(const char **input, t_token **head)
 {
 	if (**input == '>' && *(*input + 1) == '>')
 	{
 		add_token(head, init_token(TOKEN_DOUBLE_REDIRECT_OUT, ">>"));
 		*input += 2;
-		return ;
+		return;
 	}
 	else if (**input == '<' && *(*input + 1) == '<')
 	{
 		add_token(head, init_token(TOKEN_HEREDOC, "<<"));
 		*input += 2;
-		return ;
+		return;
 	}
 }
 
-void	add_token_based_on_char(const char **input, t_token **head)
+/**
+ * @brief Adds a token based on the current character in the input string.
+ *
+ * @param input The input string.
+ * @param head The head of the token list.
+ */
+void add_token_based_on_char(const char **input, t_token **head)
 {
 	identify_double_char_tokens(input, head);
 	if (**input == '>')
@@ -57,20 +85,30 @@ void	add_token_based_on_char(const char **input, t_token **head)
 		add_word_token(input, head);
 }
 
+/**
+ * @brief Tokenizes the input string and creates a linked list of tokens.
+ *
+ * takes a null-terminated string as input and tokenizes it based on whitespace characters.
+ * It creates a linked list of tokens, where each token represents a substring of the input string.
+ *
+ * @param input The input string to be tokenized.
+ * @return A pointer to the head of the token linked list.
+ */
 t_token	*tokenize(const char *input)
 {
 	t_token	*head;
 
 	head = NULL;
-	while (*input)
-	{
-		while (*input && ft_isspace(*input))
-			input++;
-		if (*input == '\0')
-			break ;
-		identify_and_add_token(&input, &head);
-		if (*input)
-			input++;
-	}
+	if (input)
+		while (*input) 
+		{
+			while (*input && ft_isspace(*input))
+				input++;
+			if (*input == '\0')
+				break ;
+			identify_and_add_token(&input, &head);
+			if (*input)
+				input++;
+		}
 	return (head);
 }
