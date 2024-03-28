b1558c6f684f6632e51bd58f1058b44470024673
yvann-hub <barbot.yvann@gmail.com>
2024-03-28 11:47:56 +0100

    Refactor: export builtin

--- mini-shell/src/builtins/builtins_env.c
+++ mini-shell/src/builtins/builtins_env.c
@@ -39,12 +39,19 @@ static int	update_env_var(char *key, char *value, char ***env)
 	i = 0;
 	while ((*env)[i])
 	{
-		if (ft_strncmp((*env)[i], key, 
+		if (ft_strncmp((*env)[i], key,
 			ft_strlen(key)) == 0 && (*env)[i][ft_strlen(key)] == '=')
 		{
 			free((*env)[i]);
 			new_var = ft_strjoin(key, "=");
+			if (!new_var)
+				return (0);
 			(*env)[i] = ft_strjoin(new_var, value);
+			if (!(*env)[i])
+			{
+				free(new_var);
+				return (0);
+			}
 			free(new_var);
 			return (1);
 		}
@@ -53,35 +60,60 @@ static int	update_env_var(char *key, char *value, char ***env)
 	return (0);
 }
 
+static void	create_new_env_var(char *key, char *value, char **env_var)
+{
+	char *new_var;
+
+	new_var = ft_strjoin(key, "=");
+	if (!new_var)
+		return ;
+	*env_var = ft_strjoin(new_var, value);
+	if (!*env_var)
+	{
+		free(new_var);
+		return ;
+	}
+	free(new_var);
+}
+
+static void	insert_env_var(char ***env, char *env_var, size_t env_size)
+{
+	char **new_env;
+
+	new_env = ft_realloc(*env, sizeof(char *) * (env_size + 1),
+			 sizeof(char *) * (env_size + 2));
+	if (!new_env)
+	{
+		free(env_var);
+		return;
+	}
+	new_env[env_size] = env_var;
+    new_env[env_size + 1] = NULL;
+    *env = new_env;
+}
+
 static void	add_new_env_var(char *arg, char ***env, size_t env_size)
 {
-	char	**split_arg;
-	char	*key;
-	char	*value;
-	char	*new_var;
-	char	**new_env;
+	char **split_arg;
+	char *key;
+	char *value;
+	char *env_var;
 
 	split_arg = ft_split(arg, '=');
+	if (!split_arg || !split_arg[0])
+	{
+		ft_free_tab(split_arg);
+		return ;
+	}
 	key = split_arg[0];
 	if (split_arg[1])
 		value = split_arg[1];
 	else
 		value = "";
-	new_env = NULL;
 	if (!update_env_var(key, value, env))
 	{
-		new_env = ft_realloc(*env, sizeof(char *) * (env_size + 1),
-				sizeof(char *) * (env_size + 2));
-		if (!new_env)
-		{
-			ft_free_tab(split_arg);
-			return ;
-		}
-		*env = new_env;
-		new_var = ft_strjoin(key, "=");
-		(*env)[env_size] = ft_strjoin(new_var, value);
-		(*env)[env_size + 1] = NULL;
-		//free(new_var);
+		create_new_env_var(key, value, &env_var);
+		insert_env_var(env, env_var, env_size);
 	}
 	ft_free_tab(split_arg);
 }
