e8556640a3da306b735aaf103f2e55b652c3d878
yvann-hub <barbot.yvann@gmail.com>
2024-05-01 13:36:06 +0200

    Fix: quotes before letters

--- mini-shell/src/tokenization/tokenization_utils.c
+++ mini-shell/src/tokenization/tokenization_utils.c
@@ -17,36 +17,39 @@ static	char	*process_quoted_content(const char *quoted_part,
 	return (value);
 }
 
-void	add_quoted_token(const char **input, t_token **head,
-			char quote_type, t_minishell *shell)
-{
-	const char	*start;
-	size_t		len;
-	char		*quoted_part;
-	char		*value;
+void add_quoted_token(const char **input, t_token **head, char quote_type, t_minishell *shell) {
+    const char *start;
+    size_t len = 0;
+    char *quoted_part;
 
-	(*input)++;
-	start = *input;
-	len = 0;
-	while ((*input)[len] && (*input)[len] != quote_type)
-		len++;
-	if ((*input)[len] == quote_type)
-	{
-		quoted_part = ft_strndup(start, len);
-		value = process_quoted_content(quoted_part, quote_type, shell);
-		add_token(head, init_token(TOKEN_WORD, value));
-		free(quoted_part);
-		free(value);
-		*input += len + 1;
-		if (**input == quote_type)
-			add_quoted_token(input, head, quote_type, shell);
-	}
-	else
-	{
-		ft_printf("minishell: syntax error: missing quotes\n");
-		free_tokens(head);
-	}
+    (*input)++;
+    start = *input;
+
+    // Count the length until the closing quote or end of string
+    while ((*input)[len] && (*input)[len] != quote_type) {
+        len++;
+    }
+
+    // Handle empty quotes
+    if (len == 0) {
+        add_token(head, init_token(TOKEN_WORD, ""));
+    } else {
+        quoted_part = ft_strndup(start, len);
+        char *value = process_quoted_content(quoted_part, quote_type, shell);
+        add_token(head, init_token(TOKEN_WORD, value));
+        free(quoted_part);
+        free(value);
+    }
+
+    // Move past the closing quote if it exists
+    if ((*input)[len] == quote_type) {
+        *input += len;
+    } else {
+        ft_printf("minishell: syntax error: missing closing quote '%c'\n", quote_type);
+        free_tokens(head); // Freeing all tokens because of syntax error
+    }
 }
+
 
 void	handle_quotes(const char **input, t_token **head,
 			t_minishell *shell, const char **start)
