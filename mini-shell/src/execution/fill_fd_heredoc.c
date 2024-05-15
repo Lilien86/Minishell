970c5b19695ae7396443e807275dcb70920aa58d
lilien86 <lilienauger.86@gmail.com>
2024-05-15 10:56:09 +0200

    fix; delet print

--- mini-shell/src/execution/fill_fd_heredoc.c
+++ mini-shell/src/execution/fill_fd_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fill_fd_heredoc.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lauger <lauger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 09:43:37 by lauger            #+#    #+#             */
-/*   Updated: 2024/05/14 09:09:43 by marvin           ###   ########.fr       */
+/*   Updated: 2024/05/15 10:55:12 by lauger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void	handle_here_doc(t_minishell *shell, t_file here_doc, char *delimiter)
 		here_doc_content = temp;
 	}
 	write_here_doc_in_file(here_doc_content, here_doc.fd, shell);
-	here_doc_replace_var_env(here_doc_content, shell);
+	//here_doc_replace_var_env(here_doc_content, shell);
 	free_minishell(shell);
 	shell->exit_status = 0;
 	close(here_doc.fd);
--- mini-shell/src/execution/utils_to_exec.c
+++ mini-shell/src/execution/utils_to_exec.c
@@ -2,58 +2,58 @@
 
 void    print_pos_dollars(t_pos_len *dollars, int size)
 {
-    int     i;
+	int     i;
 
-    i = 0;
-    while (i < size)
-    {
-        printf("dollars[%d] --> pos/%d size/%d\n", i, dollars[i].pos, dollars[i].len);
-        i++;
-    }
-    return ;
+	i = 0;
+	while (i < size)
+	{
+		printf("dollars[%d] --> pos/%d size/%d\n", i, dollars[i].pos, dollars[i].len);
+		i++;
+	}
+	return ;
 }
 
 int  counter_dollars(const   char *content)
 {
-    int     counter;
-    int     i;
+	int     counter;
+	int     i;
 
-    counter = 0;
-    i = 0;
-     while(content[i] != '\0')
-    {
-        if (content[i] == '$')
-        {
-            counter++;
-        }
-        i++;
-    }
-    return (counter);
+	counter = 0;
+	i = 0;
+	 while(content[i] != '\0')
+	{
+		if (content[i] == '$')
+		{
+			counter++;
+		}
+		i++;
+	}
+	return (counter);
 }
 
 void print_list(t_list *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        printf("%d: %s\n", i++, (char *)list->content);
-        list = list->next;
-    }
+	i = 0;
+	while (list)
+	{
+		printf("%d: %s\n", i++, (char *)list->content);
+		list = list->next;
+	}
 }
 
 int  len_to_dollars(const char *content, int index)
 {
-    int     len;
+	int     len;
 
-    len = 0;
-    if (content[index] != '$')
-        return (0);
-    while (content[index] != ' ' && content[index] != '\0')
-    {
-        index++;
-        len ++;
-    }
-    return (len);
+	len = 0;
+	if (content[index] != '$')
+		return (0);
+	while (content[index] != ' ' && content[index] != '\0')
+	{
+		index++;
+		len ++;
+	}
+	return (len);
 }
