85c49a48d0f86e34f3abc890ca5d61e0dde87050
yvann-hub <barbot.yvann@gmail.com>
2024-05-27 10:54:05 +0200

    Norminette: decoupe existing var,

--- mini-shell/src/builtins/env/ft_export2.c
+++ mini-shell/src/builtins/env/ft_export2.c
@@ -20,6 +20,34 @@ int	is_valid_var_name(const char *var, t_minishell *shell)
 	return (1);
 }
 
+char	*handle_plus_equal(char *env_var, char *var)
+{
+	char	*eq_pos;
+	char	*value_to_add;
+	char	*existing_value;
+	char	*new_value;
+
+	eq_pos = ft_strchr(var, '=');
+	if (eq_pos != NULL)
+		value_to_add = eq_pos + 1;
+	else
+		value_to_add = "";
+	if (ft_strchr(env_var, '='))
+	{
+		existing_value = ft_strchr(env_var, '=') + 1;
+		if (existing_value == NULL)
+			return (NULL);
+	}
+	else
+		return (NULL);
+	new_value = malloc(ft_strlen(existing_value) + ft_strlen(value_to_add) + 1);
+	if (new_value == NULL)
+		return (NULL);
+	ft_strcpy(new_value, existing_value);
+	ft_strcat(new_value, value_to_add);
+	return (new_value);
+}
+
 int	handle_export_token(t_token *token, char ***env, t_minishell *shell)
 {
 	(void)shell;
