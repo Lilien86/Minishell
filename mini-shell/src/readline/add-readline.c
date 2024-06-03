2d16ac8aad83892b2e6e24541175adc502ce208f
lilien86 <lilienauger.86@gmail.com>
2024-06-03 11:20:55 +0200

    fix; remove printf

--- mini-shell/src/readline/add-readline.c
+++ mini-shell/src/readline/add-readline.c
@@ -6,7 +6,7 @@
 /*   By: lauger <lauger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 09:52:11 by ybarbot           #+#    #+#             */
-/*   Updated: 2024/06/03 11:19:31 by lauger           ###   ########.fr       */
+/*   Updated: 2024/06/03 11:20:39 by lauger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ void	process_input(t_minishell *shell)
 	shell->history[shell->history_index] = ft_strdup(shell->input);
 	shell->history_index = (shell->history_index + 1) % MAX_HISTORY_SIZE;
 	shell->tokens = tokenize(shell->input, shell->env, shell);
-	debug_print_tokens(shell->tokens);
+	//debug_print_tokens(shell->tokens);
 	if (shell->tokens)
 		execute_input_commands(shell);
 }
